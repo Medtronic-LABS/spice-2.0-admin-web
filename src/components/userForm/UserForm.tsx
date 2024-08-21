f1f5529feb07f30e3fad1ce419a71b28009fa6dc
Nithin A N <nithinan@ideas2it.com>
2024-08-21 17:18:05 +0530

    MDT-15348: Peer Supervisor can act as CHW also
      - Changed the logic to allow peer supervisor to act as CHW as well
      - Removed the delete option for existing roles in User edit

--- src/components/userForm/UserForm.tsx
+++ src/components/userForm/UserForm.tsx
@@ -258,28 +258,8 @@ const UserForm = ({
     () => [
       {
         ...initialEditValue,
-        hfTenantIds: isEdit ? (initialEditValue?.organizations || []).map((org: any) => org.id) : [],
-        suiteAccess: initialEditValue?.suiteAccess?.map((org: any) => ({
-          ...org,
-          isFixed: APPCONSTANTS.spiceRole.spiceInsights !== org.groupName
-        })),
-        culture:
-          !isCultureListLoading &&
-          cultureList?.find(
-            (culture: { id: any }) => culture.id === (initialEditValue?.cultureId || APPCONSTANTS.DEFAULT_CULTURE.id)
-          ),
-        district:
-          initialEditValue?.organizations.filter(
-            (countyDetail: any) => countyDetail.formName === NAMING_VARIABLES.district
-          ) || '',
-        chiefdom:
-          initialEditValue?.organizations.filter(
-            (countyDetail: any) => countyDetail.formName === NAMING_VARIABLES.chiefdom
-          ) || '',
-        healthfacility:
-          initialEditValue?.organizations.filter(
-            (countyDetail: any) => countyDetail.formName === NAMING_VARIABLES.healthFacility
-          ) || ''
+        selectedRoles: initialEditValue?.role || [],
+        hfTenantIds: isEdit ? (initialEditValue?.organizations || []).map((org: any) => org.id) : []
       }
     ],
     [cultureList, initialEditValue, isCultureListLoading, isEdit]
@@ -516,6 +496,28 @@ const UserForm = ({
     return !isLastChild && <div className='divider mx-neg-1dot25 mb-1dot5' />;
   };
 
+  const mobileRoles = useMemo(() => ['CHW'], []);
+  const newMobileRoles = useMemo(() => ['CHW', 'PEER_SUPERVISOR'], []);
+  const adminRoles = useMemo(
+    () => ['HEALTH_FACILITY_ADMIN', 'PROVIDER', 'MID_WIFE', 'LAB_ASSISTANT', 'SRN', 'SECHN', 'CHA', 'MCHA'],
+    []
+  );
+  const superAdminRoles = useMemo(() => ['SUPER_ADMIN'], []);
+  const hfCreateRoles = useMemo(
+    () => [
+      'HEALTH_FACILITY_ADMIN',
+      'PROVIDER',
+      'MID_WIFE',
+      'LAB_ASSISTANT',
+      'SRN',
+      'SECHN',
+      'CHA',
+      'MCHA',
+      'PEER_SUPERVISOR'
+    ],
+    []
+  );
+
   // roles based CHW related utils
   const selectedRoles = useCallback((index: number) => form.getState().values?.users?.[index]?.roles, [form]);
 
@@ -542,10 +544,10 @@ const UserForm = ({
       const newDisabledRoles = [...disabledRoles.current];
       let validRoles: string[] = [];
       const selectedAllRoles = [...(selectedRoles(index) || [])];
-      if (selectedAllRoles.some((ro: IRoles) => mobileRoles.includes(ro.name))) {
-        validRoles = mobileRoles;
-      } else if (selectedAllRoles.some((ro: IRoles) => peerSupervisorRoles.includes(ro.name))) {
-        validRoles = peerSupervisorRoles;
+      if (selectedAllRoles.some((ro: IRoles) => newMobileRoles.includes(ro.name))) {
+        validRoles = newMobileRoles;
+      } else if (selectedAllRoles.some((ro: IRoles) => adminRoles.includes(ro.name))) {
+        validRoles = adminRoles;
       } else if (selectedAllRoles.some((ro: IRoles) => superAdminRoles.includes(ro.name))) {
         validRoles = superAdminRoles;
       } else if (selectedAllRoles.some((ro: IRoles) => reportAdminRole.includes(ro.name))) {
@@ -564,7 +566,7 @@ const UserForm = ({
       isCHPSelected,
       isHF,
       isHFCreate,
-      onlyCHWRoles,
+      mobileRoles,
       rolesGrouped,
       selectedRoles,
       superAdminRoles,
@@ -935,9 +937,19 @@ const UserForm = ({
                             placeholder=''
                             isModel={true}
                             isMulti={true}
+                            isOptionDisabled={(option: any) => {
+                              const optionsToBeDisabled = [
+                                ...(mandatoryRoles ? mandatoryRoles : []),
+                                ...(disabledRoles.current[index] || [])
+                              ];
+                              return optionsToBeDisabled.length
+                                ? optionsToBeDisabled.map((v: any) => v.id).includes(option.id)
+                                : null;
+                            }}
                             required={true}
-                            options={getAdminRoles()}
-                            mandatoryOptions={autoFetched[index] ? mandatoryRoles : []}
+                            options={roleOptions.current?.[index] || []}
+                            mandatoryOptions={mandatoryRoles ? mandatoryRoles : []}
+                            disabledOptions={disabledRoles.current[index]}
                             loading={isRolesLoading}
                             error={isError(meta) && !spiceRole?.length}
                             onChange={(values: any, { option }: { option: any }) => {
